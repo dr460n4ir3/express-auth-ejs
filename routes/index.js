67648b981fd6580c0bd61d6d88de2f65a1971e6c
dr460n4ir3 <token.b@gmail.com>
2023-05-16 01:46:49 -0400

    made nav a componet, created about & contact form

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,26 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/about', (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render('about', { 
+        title: 'About Us',
+        text: 'This is the about page',
+        isAuthenticated: req.oidc.isAuthenticated(),
+        user: req.oidc.user, 
+    });
+});
+
+router.get('/contact', (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render('contact', { 
+        title: 'Contact Us',
+        text: 'This is the contact page',
+        isAuthenticated: req.oidc.isAuthenticated(),
+        user: req.oidc.user, 
+    });
+});
+
 /*router.get('/login', (req, res) => {
     
     res.render('index', { title: 'Login Demo', year: 2023 });
